7f0125254cc367b5d3c6d1df016c9ba7d07d8748
VictorDu <wentao@signalfuse.com>
2015-08-05 09:00:11 -0700

    Fix the wrong URL in README.md, default attribute in default.rb and metadata.rb
    -- change the README.md - Add more information about how to use chef recipes.
    -- change the README.md - Fix the new generated Ingest URL.
    -- change the attributes/default.rb - The default ["write_http"]["set_aws_instanceId"] atrribute is false.
    -- change the metadata.rb. - Add support os "ubuntu", change the email to "support+chef@signalfx.com"

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'chef_install_configure_collectd'
 maintainer 'SignalFx, Inc.'
-maintainer_email 'support@signalfx.com'
+maintainer_email 'support+chef@signalfx.com'
 license 'Copyright SignalFx, Inc. All rights reserved'
 description 'This cookbook provides a set of recipes to install and cofigure the collectd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -8,6 +8,7 @@ version '0.1.0'
 
 supports "centos"
 supports "amazon"
+supports "ubuntu"
 
 recipe "chef_install_configure_collectd::default",
   "Install the lastest version collectd of SignalFx, Inc"
