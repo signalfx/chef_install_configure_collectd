d983384747c9a8bf210896e882e3ecfbccde1ac5
Drew Griffin <drew.griffin@signalfx.com>
2016-04-05 16:52:33 -0400

    add foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'chef_install_configure_collectd'
 maintainer 'SignalFx, Inc.'
 maintainer_email 'support+chef@signalfx.com'
+source_url 'https://github.com/signalfx/chef_install_configure_collectd'
+issues_url 'https://github.com/signalfx/chef_install_configure_collectd/issues'
 license 'Copyright SignalFx, Inc. All rights reserved'
 description 'This cookbook provides a set of recipes to install and cofigure the collectd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
