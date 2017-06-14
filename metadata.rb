275319bb0b5f45914c27d0aa70b468e0894e810c
l.h. riley <liam.h.riley@gmail.com>
2017-06-14 11:09:04 -0700

    Updating / pinning versions.

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,14 @@ issues_url 'https://github.com/signalfx/chef_install_configure_collectd/issues'
 license 'Copyright SignalFx, Inc. All rights reserved'
 description 'Use this cookbook to install the SignalFx collectd agent and collectd plugins.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.6'
+version '0.3.0'
+
+chef_version '~> 12.5'
 
 supports "centos"
 supports "amazon"
 supports "ubuntu"
 supports "debian"
 
-depends 'apt', '>= 2.8'
+depends 'apt', '< 6.0'
 depends 'poise-python', '>= 1.6'
